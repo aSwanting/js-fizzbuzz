3d9e5f541990109f377851ac9058c16736bba94e
aSwanting <gab.damico@gmail.com>
2023-10-06 16:07:04 +0200

    refactor: replaced n with (i+1)

--- js/script.js
+++ js/script.js
@@ -4,24 +4,22 @@
 for (let i = 0; i < 100; i++) {
     
     const container = document.querySelector(".grid")
-    const newDiv = document.createElement("div")
+    const newDiv = document.createElement("div")    
 
-    n = i + 1
-
-    if (n % 3 === 0 && n % 5 === 0) {
+    if ((i+1) % 3 === 0 && (i+1) % 5 === 0) {
         newDiv.append('FizzBuzz')
         newDiv.className = "block fizz-buzz"
 
-    } else if (n % 3 === 0) {
+    } else if ((i+1) % 3 === 0) {
         newDiv.append('Fizz')
         newDiv.className = "block fizz"
 
-    } else if (n % 5 === 0) {
+    } else if ((i+1) % 5 === 0) {
         newDiv.append('Buzz')
         newDiv.className = "block buzz"
 
     } else {
-        newDiv.append(n)
+        newDiv.append(i+1)
         newDiv.className = "block"
     }
 
