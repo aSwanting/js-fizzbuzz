dd679f79eb9cf7abe35425f0000b9e5436ea1b56
aSwanting <gab.damico@gmail.com>
2023-10-06 14:15:28 +0200

    Bonus 1: add elements to DOM using append()

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
+
+const container = document.querySelector(".container")
+
 for (let i = 0; i < 100; i++) {
 
     n = i + 1
 
-    if (n % 3 === 0 && n % 5 === 0) {
-        console.log(n + ' FizzBuzz')
+    if (n % 3 === 0 && n % 5 === 0) {      
 
-    } else if (n % 3 === 0) {
-        console.log(n + ' Fizz')
+        const newDiv = document.createElement("div")
+        newDiv.append('FizzBuzz (' + n + ')')
+        newDiv.className = "block FizzBuzz"
+        container.append(newDiv)
 
-    } else if (n % 5 === 0) {
-        console.log(n + ' Buzz')
+    } else if (n % 3 === 0) {   
+
+        const newDiv = document.createElement("div")
+        newDiv.append('Fizz (' + n + ')')
+        newDiv.className = "block Fizz"
+        container.append(newDiv)
+
+    } else if (n % 5 === 0) {  
+
+        const newDiv = document.createElement("div")
+        newDiv.append('Buzz (' + n + ')')
+        newDiv.className = "block Buzz"
+        container.append(newDiv)
 
     } else {
-        console.log(n)
+        const newDiv = document.createElement("div")
+        newDiv.append(n)      
+        newDiv.className = "block"  
+        container.append(newDiv)
     }
-
 
 }
